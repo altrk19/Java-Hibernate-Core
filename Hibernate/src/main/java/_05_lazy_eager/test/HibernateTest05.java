7f84be4aaecc9ccc10d30619c3c7579c3e6075b8
aturk <aturk@netas.com.tr>
2020-08-16 15:45:39 +0300

    added eager and lazy

---
+++ Hibernate/src/main/java/_05_lazy_eager/test/HibernateTest05.java
@@ -0,0 +1,85 @@
+package _05_lazy_eager.test;
+
+import _05_lazy_eager.model.AddressEntity5;
+import _05_lazy_eager.model.UserEntity5Eager;
+import _05_lazy_eager.model.UserEntity5Lazy;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateTest05 {
+
+    private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public static void main(String[] args) {
+        insertUserLazy();
+        insertUserEager();
+    }
+
+    private static void insertUserLazy() {
+        UserEntity5Lazy user = new UserEntity5Lazy();
+        user.setUserName("Ali");
+
+        AddressEntity5 addr1 = new AddressEntity5();
+        addr1.setStreet("Street 1");
+        addr1.setCity("City 1");
+        addr1.setPostCode("postcode 1");
+
+        user.getListOfAddresses().add(addr1);
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+
+        session = sessionFactory.openSession();
+        //we are getting the user object without the addresses
+        user = session.get(UserEntity5Lazy.class, 1);
+        session.close();
+
+        System.out.println("user name : " + user.getUserName());
+
+
+
+
+        //the following line fails beacuse of lazy initialization.
+        //System.out.println(user.getListOfAddresses());
+
+
+    }
+
+    private static void insertUserEager() {
+        UserEntity5Eager user = new UserEntity5Eager();
+        user.setUserName("Umut");
+
+        AddressEntity5 addr1 = new AddressEntity5();
+        addr1.setStreet("Street 3");
+        addr1.setCity("City 3");
+        addr1.setPostCode("postcode 3");
+
+        user.getListOfAddresses().add(addr1);
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+
+        session = sessionFactory.openSession();
+        //we are getting the user object without the addresses
+        user = session.get(UserEntity5Eager.class, 1);
+        System.out.println(user);
+        session.close();
+
+        System.out.println("user name : " + user.getUserName());
+
+
+
+        //now it is eager initialization
+        System.out.println(user.getListOfAddresses());
+
+
+    }
+
+}
