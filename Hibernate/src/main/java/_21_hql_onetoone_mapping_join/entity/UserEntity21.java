cb552032c4a540eb24d0101193da9ca573ffbb1d
aturk <aturk@netas.com.tr>
2020-08-23 21:01:35 +0300

    added hql onetoone

---
+++ Hibernate/src/main/java/_21_hql_onetoone_mapping_join/entity/UserEntity21.java
@@ -0,0 +1,51 @@
+package _21_hql_onetoone_mapping_join.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_entity21")
+public class UserEntity21 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    private String userName;
+
+    @OneToOne
+    @JoinColumn(name = "vehicle_id")
+    private VehicleEntity21 vehicle;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public VehicleEntity21 getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(VehicleEntity21 vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity21{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", vehicle=" + vehicle +
+                '}';
+    }
+}
